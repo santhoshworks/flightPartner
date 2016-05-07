c617f44e18a50f97b67e57629b06cc2d4db5b65e
santhoshworks <santhoshp.j4@gmail.com>
2016-05-07 15:47:15 -0400

    Added Filter Change

--- routes/api.js
+++ routes/api.js
@@ -14,11 +14,11 @@ var Trip  = require('../models/trip');
 //Product.methods(['get', 'post', 'put','delete']);
 //Product.register(router, '/products')
 
-//User.methods(['get', 'post', 'put','delete']);
-//User.register(router, '/users')
+User.methods(['get', 'post', 'put','delete']);
+User.register(router, '/users')
 
-//Trip.methods(['get', 'post', 'put','delete']);
-//Trip.register(router, '/trips')
+Trip.methods(['get', 'post', 'put','delete']);
+Trip.register(router, '/trips')
 
 
 // Routes
@@ -27,6 +27,21 @@ router.use(function(req, res, next) {
     console.log('Something is happening.');
     next(); // make sure we go to the next routes and don't stop here
 });
+
+//Sample Filter request
+//http://localhost:5000/api/trips/filter?where[to]=Chennai&where[from]=Jacksonville
+router.route('/trips/filter').get(function (req,res) {
+    var filterObj= req.query.where || {};
+    console.log(req.query.where)
+
+    Trip.find(filterObj, function(err, trips) {
+        if (err)
+            res.send(err);
+
+        res.json(trips);
+    });
+});
+/*
 
 router.route('/users')
 
@@ -55,6 +70,37 @@ router.route('/users')
             res.json(users);
         });
     });
+*/
+
+/*
+
+router.route('/trips')
+
+    // create a bear (accessed at POST http://localhost:8080/api/bears)
+    .post(function(req, res) {
+        var trip = new Trip();      // create a new instance of the Bear model
+        trip.userID = req.body.userID;  // set the bears name (comes from the request
+        trip.from = req.body.from;
+        trip.to = req.body.to;
+        trip.startDate = req.body.startDate;
+        trip.flightCode = req.body.flightCode;
+
+        trip.save(function(err) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'Trip added!' });
+        });
+
+    })
+    .get(function(req, res) {
+        Trip.find( function(err, trips) {
+            if (err)
+                res.send(err);
+
+            res.json(trips);
+        });
+    });*/
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });
